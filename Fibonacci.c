4c6bf56a24dd18e72dfdd16d38f5b0c003bc0e49
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2016-10-01 15:56:10 -0700

    Fibonacci Series
    Template.

---
+++ Fibonacci.c
@@ -0,0 +1,18 @@
+#include <iomanip>
+#include <unistdl.h>
+#include <cmath.h>
+
+
+// --------------------------------------------------------- //
+// Title:     Fibonacci Series
+// Author:    Jason Ghiraldini
+// Term:      Fall 2016
+// Class:     CS 325 - Analysis of Algorithms
+// Input:     User inputs the number of the sequence
+// Output:    Code computes Fibonacci number
+// --------------------------------------------------------- //
+
+int main(int argc, char* argvp[]){
+
+  return 0;
+}
